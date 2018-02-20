01238a0bd8b56a74bb4a45d16e40f56d24cc5b9c
Spencer Finnell <spencerfinnell@gmail.com>
2018-02-20 14:46:09 -0500

    Setup guide enhancements.

--- resources/views/nux/admin-page.php
+++ resources/views/nux/admin-page.php
@@ -17,15 +17,15 @@ wp_enqueue_style( 'bigbox-nux' );
 ?>
 
 <div class="wrap about-wrap bigbox-setup-guide">
-	<h1><?php esc_html_e( '🎉 Welcome to BigBox', 'bigbox' ); ?></h1>
+	<h1><?php esc_html_e( '📦 Welcome to BigBox', 'bigbox' ); ?></h1>
 
 	<p class="about-text">
 		<?php echo wp_kses_post( __( 'Use the steps below to finish setting up your new website.', 'bigbox' ) ); ?>
 	</p>
 
 	<p class="helpful-links">
-		<a href="" class="button button-primary js-trigger-documentation" target="_blank"><?php esc_html_e( 'Search Documentation', 'bigbox' ); ?></a>&nbsp;
-		<a href="" class="button button-secondary" target="_blank"><?php esc_html_e( 'Contact Support', 'bigbox' ); ?></a>&nbsp;
+		<a href="https://docs.bigbowc.com/" class="button button-primary" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Search Documentation', 'bigbox' ); ?></a>&nbsp;
+		<a href="https://bigboxwc.com/account/support" class="button button-secondary" target="_blank" rel="noopener noreferrer"><?php esc_html_e( 'Contact Support', 'bigbox' ); ?></a>
 	</p>
 </div>
 
--- resources/views/nux/steps/license-manager.php
+++ resources/views/nux/steps/license-manager.php
@@ -16,6 +16,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 wp_enqueue_script( 'bigbox-license-manager' );
 ?>
 
-<p><?php esc_html_e( 'Please enter the license key received with your purchase to enable automatic updates and ensure your website stays up to date and secure.', 'bigbox' ); ?></p>
+<p>
+<?php
+// Translators: %1$s Opening HTML <a> tag. Do not translate. %2$s Closing HTML tag. Do not transslate.
+echo wp_kses_post( sprintf( __( 'Please enter the license key received with your purchase to enable automatic updates and ensure your website stays up to date and secure. You can find your license key on your %1$sBigBox account page%2$s.', 'bigbox' ), '<a href="https://bigboxwc.com/account">', '</a>' ) );
+?>
+</p>
 
-<div id="bigbox-license-manager"></div>
+<div id="bigbox-license-manager"><?php esc_html_e( 'Unable to load license manager. Please enable Javascript.', 'bigbox' ); ?></div>
