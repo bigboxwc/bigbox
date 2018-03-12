588a51a9a9cca514110109e96cda67279b9a5232
Spencer Finnell <spencerfinnell@gmail.com>
2018-03-12 17:03:45 -0400

    Start single layout

--- app/integrations/woocommerce/template-hooks.php
+++ app/integrations/woocommerce/template-hooks.php
@@ -81,6 +81,8 @@ add_action( 'the_post', function() {
 	}
 } );
 
+remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash' );
+
 /**
  * @see wc-formatting-functions.php
  */
---
+++ app/integrations/woocommerce/views/single-product/tabs/tabs.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Single Product tabs
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/tabs/tabs.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Filter tabs and allow third parties to add their own.
+ *
+ * Each tab is an array containing title, callback and priority.
+ * @see woocommerce_default_product_tabs()
+ */
+$tabs = apply_filters( 'woocommerce_product_tabs', array() );
+
+if ( ! empty( $tabs ) ) : ?>
+	
+	<div class="product-data">
+
+	<?php foreach ( $tabs as $key => $tab ) : ?>
+
+		<div class="product-product__section" id="tab-<?php echo esc_attr( $key ); ?>">
+			<?php
+			if ( isset( $tab['callback'] ) ) :
+				call_user_func( $tab['callback'], $key, $tab );
+			endif;
+			?>
+		</div>
+
+	<?php endforeach; ?>
+
+	</div>
+
+<?php endif; ?>
