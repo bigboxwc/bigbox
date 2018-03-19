0c2cea206bea41385bc3dea515a1a0be82490d37
Spencer Finnell <spencerfinnell@gmail.com>
2018-03-19 15:29:13 -0400

    Start blog index

--- resources/views/layout/index.php
+++ resources/views/layout/index.php
@@ -18,7 +18,7 @@ bigbox_view( 'global/header' );
 ?>
 
 <div id="main" class="site-primary" role="main">
-	Hi
+	<h1 class="page-title"><?php echo get_option( 'page_for_posts' ) ? esc_html( get_the_title( get_option( 'page_for_posts' ) ) ) : esc_html__( 'Blog', 'bigbox' ); ?></h1>
 </div>
 
 <?php
