dbc822acf3d6e8c292ceb0c47656339bb84c1e12
Spencer Finnell <spencerfinnell@gmail.com>
2018-03-23 09:57:35 -0400

    FacetWP pagination, results, sorting integration.

--- app/integrations/facetwp/template-functions.php
+++ app/integrations/facetwp/template-functions.php
@@ -21,3 +21,67 @@ if ( ! defined( 'ABSPATH' ) ) {
 function bigbox_facetwp_pagination() {
 	echo do_shortcode( '[facetwp pager="true"]' );
 }
+
+/**
+ * Modify the output of the pagination.
+ *
+ * @since 1.0.0
+ *
+ * @param string $output Current output.
+ * @param array  $params An associative array of result count settings.
+ * @return string
+ */
+function bigbox_facetwp_pagination_output( $output, $params ) {
+	$page        = $params['page'];
+	$total_pages = $params['total_pages'];
+
+	$prev = '';
+	$next = '';
+
+	if ( $page > 1 ) {
+		$prev = '<a data-page="' . ( $page - 1 ) . '" class="facetwp-page prev">' . esc_html__( '&larr; Previous Page', 'bigbox' ) . '</a>';
+	}
+
+	if ( $page < $total_pages && $total_pages > 1 ) {
+		$next = '<a data-page="' . ( $page + 1 ) . '" class="facetwp-page next">' . esc_html__( 'Next Page &rarr;', 'bigbox' ) . '</a>';
+	}
+
+	return $prev . $output . $next;
+}
+
+/**
+ * Output FacetWP result count.
+ *
+ * @since 1.0.0
+ */
+function bigbox_facetwp_result_count() {
+	echo do_shortcode( '[facetwp counts="true"]' );
+}
+
+/**
+ * Modify the output of the result count.
+ *
+ * @since 1.0.0
+ *
+ * @param string $output Current output.
+ * @param array  $params An associative array of result count settings.
+ * @return string
+ */
+function bigbox_facetwp_result_count_output( $output, $params ) {
+	return sprintf( 
+		// Translators: %1$s Lower count. %2$s Upper count. %3$s Total count.
+		__( '%1$s-%2$s of %3$s results', 'bigbox' ),
+		$params['lower'],
+		$params['upper'],
+		$params['total']
+	);
+}
+
+/**
+ * Output FacetWP catalog ordering.
+ *
+ * @since 1.0.0
+ */
+function bigbox_facetwp_catalog_ordering() {
+	echo do_shortcode( '[facetwp sort="true"]' );
+}
--- app/integrations/facetwp/template-hooks.php
+++ app/integrations/facetwp/template-hooks.php
@@ -16,4 +16,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * @see woocommerce/templates/archive-product.php.
  */
+
+// Add pagination.
+remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination' );
 add_action( 'woocommerce_after_shop_loop', 'bigbox_facetwp_pagination' );
+
+add_filter( 'facetwp_pager_html', 'bigbox_facetwp_pagination_output', 10, 2 );
+
+// Replace WooCommerce's "Results Showing" and sorting options.
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+
+add_action( 'woocommerce_before_shop_loop', 'bigbox_facetwp_result_count', 20 );
+add_action( 'woocommerce_before_shop_loop', 'bigbox_facetwp_catalog_ordering', 30 );
+
+add_filter( 'facetwp_result_count', 'bigbox_facetwp_result_count_output', 10, 2 );
