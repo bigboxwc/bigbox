f248a4d1b05822755d2c65f8930ae119842914f8
Spencer Finnell <spencerfinnell@gmail.com>
2018-03-29 19:54:01 -0400

    Add other color controls. Use as little BS as possible.

--- app/theme/customize/config-grays.php
+++ app/theme/customize/config-grays.php
@@ -12,6 +12,34 @@
 return [
 	'gray-100' => [
 		'label'   => esc_html__( 'Gray 100', 'bigbox' ),
+		'default' => '#f5f6f8',
+	],
+	'gray-200' => [
+		'label'   => esc_html__( 'Gray 200', 'bigbox' ),
 		'default' => '#ecedf2',
+	],
+	'gray-300' => [
+		'label'   => esc_html__( 'Gray 300', 'bigbox' ),
+		'default' => '#acb3c7',
+	],
+	'gray-400' => [
+		'label'   => esc_html__( 'Gray 400', 'bigbox' ),
+		'default' => '#8a94b0',
+	],
+	'gray-500' => [
+		'label'   => esc_html__( 'Gray 500', 'bigbox' ),
+		'default' => '#697599',
+	],
+	'gray-600' => [
+		'label'   => esc_html__( 'Gray 600', 'bigbox' ),
+		'default' => '#525b78',
+	],
+	'gray-700' => [
+		'label'   => esc_html__( 'Gray 700', 'bigbox' ),
+		'default' => '#3b4257',
+	],
+	'gray-800' => [
+		'label'   => esc_html__( 'Gray 800', 'bigbox' ),
+		'default' => '#242936',
 	],
 ];
--- app/theme/customize/config-scheme.php
+++ app/theme/customize/config-scheme.php
@@ -14,4 +14,32 @@ return [
 		'label'   => esc_html__( 'Primary', 'bigbox' ),
 		'default' => '#346bcc',
 	],
+	'secondary' => [
+		'label'   => esc_html__( 'Secondary', 'bigbox' ),
+		'default' => '#fd7356',
+	],
+	'success' => [
+		'label'   => esc_html__( 'Success', 'bigbox' ),
+		'default' => '#8dbc54',
+	],
+	'info' => [
+		'label'   => esc_html__( 'Information', 'bigbox' ),
+		'default' => '#17a2b8',
+	],
+	'warning' => [
+		'label'   => esc_html__( 'Warning', 'bigbox' ),
+		'default' => '#f9c95d',
+	],
+	'danger' => [
+		'label'   => esc_html__( 'Danger', 'bigbox' ),
+		'default' => '#e05f59',
+	],
+	'light' => [
+		'label'   => esc_html__( 'Light', 'bigbox' ),
+		'default' => '#fbfcff',
+	],
+	'dark' => [
+		'label'   => esc_html__( 'Dark', 'bigbox' ),
+		'default' => '#3b4257',
+	],
 ];
