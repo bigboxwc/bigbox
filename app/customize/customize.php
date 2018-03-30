81c8b58c5b0f58c67ee9a7e939ad230fe24114e2
Spencer Finnell <spencerfinnell@gmail.com>
2018-03-30 12:32:57 -0400

    Start type controls.

--- app/customize/customize.php
+++ app/customize/customize.php
@@ -180,7 +180,7 @@ function bigbox_customize_register_colors( $wp_customize ) {
 	$wp_customize->add_panel(
 		'colors', [
 			'title'    => _x( 'Colors', 'customizer panel title', 'bigbox' ),
-			'priority' => 200,
+			'priority' => 30,
 		]
 	);
 
@@ -231,6 +231,67 @@ function bigbox_customize_register_colors( $wp_customize ) {
 	}
 }
 add_action( 'customize_register', 'bigbox_customize_register_colors', 11 );
+
+/**
+ * Type controls.
+ *
+ * @since 1.0.0
+ *
+ * @param WP_Customize_Manager $wp_customize The Customizer object.
+ */
+function bigbox_customize_register_type( $wp_customize ) {
+	// Create "Scheme" and "Grays" sections.
+	$wp_customize->add_section(
+		'type', [
+			'title'    => _x( 'Typography', 'customizer section title (colors)', 'bigbox' ),
+			'priority' => 20,
+		]
+	);
+	$wp_customize->add_setting(
+		'type-font-family', [
+			'default' => 'default',
+		]
+	);
+
+	$wp_customize->add_control(
+		'type-font-family', [
+			'label'    => esc_html__( 'Font Family', 'bigbox' ),
+			'type'     => 'select',
+			'choices'  => [
+				'default' => esc_html__( 'System Default', 'bigbox' ),
+			],
+			'section'  => 'type',
+		]
+	);
+
+	$weights = [
+		'base' => esc_html__( 'Base Font Weight', 'bigbox' ),
+		'bold' => esc_html__( 'Bold Font Weight', 'bigbox' ),
+	];
+
+	foreach ( $weights as $weight => $label ) {
+		$key = "type-{$weight}";
+
+		$wp_customize->add_setting(
+			$key, [
+				'default' => 'normal',
+			]
+		);
+
+		$wp_customize->add_control(
+			$key, [
+				'label'    => $label,
+				'type'     => 'select',
+				'choices'  => [
+					'normal' => 400
+				],
+				'section'  => 'type',
+			]
+		);
+
+	}
+}
+add_action( 'customize_register', 'bigbox_customize_register_type', 11 );
 
 /**
  * Convert a HEX value to RGBA.
