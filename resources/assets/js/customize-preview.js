f1d38a4a1116a373fd95ec53a0c9b14249d2d72b
Spencer Finnell <spencerfinnell@gmail.com>
2018-03-30 15:35:01 -0400

    Simplify CSS preview.

--- resources/assets/js/customize-preview.js
+++ resources/assets/js/customize-preview.js
@@ -2,3 +2,20 @@
  * Internal dependencies.
  */
 import './customize/preview-colors.js';
+
+export const updateCss = () => {
+	wp.ajax.send( 'bigbox-preview-css', {
+		success: ( response ) => {
+
+			// Update inline CSS.
+			const selector = 'bigbox-inline-css';
+
+			$( `#${selector}` ).remove();
+
+			$( '<div>', {
+				id:   selector,
+				html: '&shy;<style>' + response + '</style>'
+			} ).appendTo( 'body' );
+		},
+	} );
+};
--- resources/assets/js/customize/preview-type.js
+++ resources/assets/js/customize/preview-type.js
@@ -15,14 +15,16 @@ import { updateCss } from './../customize-preview.js';
 
 	// Wait for Preview ready.
 	wp.customize.bind( 'preview-ready', () => {
-		const colorControls = filter( Object.keys( wp.customize.settings.activeControls ), ( setting ) => {
-			return setting.match( /color/i );
+		const typeControls = filter( Object.keys( wp.customize.settings.activeControls ), ( setting ) => {
+			return setting.match( /type/i );
 		} );
 
 		// Refresh CSS when each control changes.
 		forEach( colorControls, ( settingId ) => {
 			wp.customize( settingId, ( settingObj ) => {
-				settingObj.bind( debounce( updateCss, wp.customize.settings.timeouts.selectiveRefresh ) );
+				settingObj.bind( debounce( ( to ) => { 
+					return updateCss();
+				}, wp.customize.settings.timeouts.selectiveRefresh ) );
 			} );
 		} );
 
