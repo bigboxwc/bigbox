d44e7ca2bf2b02a59c4f5eb93270e1314fc685af
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-03 09:30:55 -0400

    CSS system tweaks.

--- resources/views/partials/navbar-primary-menu.php
+++ resources/views/partials/navbar-primary-menu.php
@@ -13,6 +13,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+$count = count( WC()->cart->get_cart_contents() );
+
 if ( ! bigbox_is_integration_active( 'woocommerce' ) ) :
 	return;
 endif;
@@ -31,8 +33,8 @@ endif;
 		</li>
 
 		<li class="navbar-menu__item navbar-menu__item--stacked">
-			<a href="<?php echo esc_url( wc_get_cart_url() ); ?>">
-				<span class="navbar-menu__cart-count"><?php echo esc_html( count( WC()->cart->get_cart_contents() ) ); ?></span>
+			<a href="<?php echo esc_url( 0 === $count ? get_permalink( wc_get_page_id( 'shop' ) ) : wc_get_cart_url() ); ?>">
+				<span class="navbar-menu__cart-count"><?php echo esc_html( $count ); ?></span>
 				<?php
 				bigbox_svg( 'cart' );
 				esc_html_e( 'Cart', 'bigbox' );
