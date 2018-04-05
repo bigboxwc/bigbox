b527be8fbce70ea11301498e07d39e65f13a5c63
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-05 17:44:55 -0400

    Tweak dynamic dropdown size.

--- resources/assets/js/navbar.js
+++ resources/assets/js/navbar.js
@@ -1,27 +1,21 @@
 /** global $ */
 
-const $real = $( '#search-dropdown-real' ).find( 'select' );
-const $fake = $( '#search-dropdown-placeholder' )
-
 /**
  * Adjust width of navbar dropdown.
  */
 export const adjustWidth = () => {
+	const $real = $( '#search-dropdown-real' ).find( 'select' );
+	const $fake = $( '#search-dropdown-placeholder' )
+
 	let selected = $real.find( 'option:selected' ).text();
 
 	if ( selected.length ) {
 		$fake.find( 'option' ).html( selected );
 	}
 
+	// Adjust width.
 	$real.width( $fake.width() );
-};
 
-$( () => {
-
-	// Adjust on change.
+	// Trigger again on change.
 	$real.change( adjustWidth );
-
-	// Adjust on load.
-	adjustWidth();
-
-} );
+};
