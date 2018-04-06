18564b53897539f955962a95250ed04ccbd54cce
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-06 08:40:41 -0400

    Use true grays as default gray color.

--- app/customize/config/colors-grays.php
+++ app/customize/config/colors-grays.php
@@ -12,34 +12,34 @@
 return [
 	'gray-100' => [
 		'label'   => esc_html__( 'Gray 100', 'bigbox' ),
-		'default' => '#f5f6f8',
+		'default' => '#f7f7f7',
 	],
 	'gray-200' => [
 		'label'   => esc_html__( 'Gray 200', 'bigbox' ),
-		'default' => '#ecedf2',
+		'default' => '#f2f2f2',
 	],
 	'gray-300' => [
 		'label'   => esc_html__( 'Gray 300', 'bigbox' ),
-		'default' => '#acb3c7',
+		'default' => '#c6c6c6',
 	],
 	'gray-400' => [
 		'label'   => esc_html__( 'Gray 400', 'bigbox' ),
-		'default' => '#8a94b0',
+		'default' => '#afafaf',
 	],
 	'gray-500' => [
 		'label'   => esc_html__( 'Gray 500', 'bigbox' ),
-		'default' => '#697599',
+		'default' => '#999999',
 	],
 	'gray-600' => [
 		'label'   => esc_html__( 'Gray 600', 'bigbox' ),
-		'default' => '#525b78',
+		'default' => '#777777',
 	],
 	'gray-700' => [
 		'label'   => esc_html__( 'Gray 700', 'bigbox' ),
-		'default' => '#3b4257',
+		'default' => '#565656',
 	],
 	'gray-800' => [
 		'label'   => esc_html__( 'Gray 800', 'bigbox' ),
-		'default' => '#242936',
+		'default' => '#353535',
 	],
 ];
