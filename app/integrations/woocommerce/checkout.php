565c7134fdb8a11bfee26afa944597d54c6113b1
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-09 16:01:14 -0400

    Some checkout fixes. See #18

--- app/integrations/woocommerce/checkout.php
+++ app/integrations/woocommerce/checkout.php
@@ -16,10 +16,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Allow these modifications to easily be removed.
 if ( apply_filters( 'bigbox_optimize_checkout', true ) ) {
 
-	// Update checkout fields.
-	add_filter( 'woocommerce_billing_fields', 'bigbox_woocommerce_billing_fields' );
-	add_filter( 'woocommerce_default_address_fields', 'bigbox_woocommerce_default_address_fields' );
-
 	// Rearrange review, payment, and totals.
 	remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
 	add_action( 'woocommerce_checkout_before_customer_details', 'woocommerce_checkout_payment', 20 );
@@ -50,57 +46,6 @@ function bigbox_woocommerce_billing_details_title( $translation, $original, $dom
 	return $translation;
 }
 add_filter( 'gettext', 'bigbox_woocommerce_billing_details_title', 10, 3 );
-
-/**
- * Adjust order for billing form fields.
- *
- * @since 1.0.0
- *
- * @param array $fields Billing fields.
- * @return array
- */
-function bigbox_woocommerce_billing_fields( $fields ) {
-	// Move email under name.
-	$fields['billing_email']['priority'] = 23;
-
-	// Remove phone.
-	unset( $fields['billing_phone'] );
-
-	return $fields;
-}
-
-/**
- * Adjust columns for billing form fields.
- *
- * @since 1.0.0
- *
- * @param array $fields Current billing fields.
- * @return array
- */
-function bigbox_woocommerce_default_address_fields( $fields ) {
-	$fields['address_1']['class'][] = 'form-row-first';
-	$fields['address_2']['class'][] = 'form-row-last';
-	$fields['address_2']['label']   = __( 'Apartment, suite, etc', 'bigbox' );
-
-	$fields['state']['priority'] = 41;
-
-	// Remove wide from some that can be collapsed.
-	foreach ( array( 'address_1', 'address_2', 'city', 'postcode' ) as $field ) {
-		$key = array_search( 'form-row-wide', $fields[ $field ]['class'], true );
-
-		if ( false !== $key ) {
-			unset( $fields[ $field ]['class'][ $key ] );
-		}
-	}
-
-	$fields['city']['class'][]     = 'form-row-first';
-	$fields['postcode']['class'][] = 'form-row-last';
-
-	// Remove company.
-	unset( $fields['company'] );
-
-	return $fields;
-}
 
 /**
  * Get cart review HTML.
