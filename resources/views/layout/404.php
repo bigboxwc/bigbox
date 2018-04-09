2fd3ebb4d0578cdeffe8463890bbc1cb5a25a46c
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-09 17:09:37 -0400

    404. Closes #17

--- resources/views/layout/404.php
+++ resources/views/layout/404.php
@@ -15,4 +15,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 bigbox_view( 'global/header' );
+?>
+
+<div id="main" class="site-primary site-primary--8">
+	<h1 class="page-title page-title--xl"><?php esc_html_e( 'Page Not Found', 'bigbox' ); ?></h1>
+	<p><?php esc_html_e( 'Try searching again!', 'bigbox' ); ?></p>
+	<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button"><?php esc_html_e( 'Go Home', 'bigbox' ); ?></a></p>
+</div>
+
+<?php
 bigbox_view( 'global/footer' );
