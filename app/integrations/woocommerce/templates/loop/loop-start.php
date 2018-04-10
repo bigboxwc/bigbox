28df94361a007d4190af503ac153cc36c8121cf8
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-09 22:14:03 -0400

    Remove defined facetwp-template

--- app/integrations/woocommerce/templates/loop/loop-start.php
+++ app/integrations/woocommerce/templates/loop/loop-start.php
@@ -21,4 +21,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<ul class="products facetwp-template columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+<ul class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
