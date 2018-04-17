f3141d98e2b45a4d43a2168bfff073eb44520029
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-17 09:28:04 -0400

    Touch events for menu items

--- resources/assets/js/app/navbar-menu.js
+++ resources/assets/js/app/navbar-menu.js
@@ -33,11 +33,55 @@ const addHoverIntent = () => {
 		sensitivity: 7,
 		interval: 90,
 	} );
-}
+};
+
+/**
+ * Touch support for tablets.
+ */
+const addTabletSupport = () => {
+	const $navigation = $( '#navbar-primary' ).find( '.navbar-menu__items' );
+
+	if ( ! $navigation.length ) {
+		return;
+	}
+
+	function toggleFocusTouchScreen() {
+		const $toggles = $navigation.find( '.menu-item-has-children > a' );
+
+		// Close when document is clicked.
+		$( document.body ).on( 'touchstart', ( e ) => {
+			if ( ! $( e.target ).closest( '.navbar-menu__items li' ).length ) {
+				$( '.navbar-menu__items li' ).removeClass( 'focus' );
+			}
+		} );
+
+		// Toggle check touched.
+		if ( 'none' === $( '.navbar-mobile-toggle--open' ).css( 'display' ) ) {
+			$toggles.on( 'touchstart', function( e ) {
+				var $el = $( this ).parent( 'li' );
+
+				if ( ! $el.hasClass( 'focus' ) ) {
+					e.preventDefault();
+
+					$el.toggleClass( 'focus' );
+					$el.siblings( '.focus' ).removeClass( 'focus' );
+				}
+			} );
+		} else {
+			$toggles.unbind( 'touchstart' );
+		}
+	};
+
+	if ( 'ontouchstart' in window ) {
+		$( window ).on( 'resize', toggleFocusTouchScreen );
+		toggleFocusTouchScreen();
+	}
+};
 
 ( function( $ ) {
 	addChildToggles();
 	addHoverIntent();
+	addTabletSupport();
 
 	// Toggle children.
 	$mobile.on( 'click', '.mobile-menu-children-toggle', function() {
