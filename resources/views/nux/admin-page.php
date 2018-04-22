9dc4221d9b63148a5d71b8a16fb608c7ebb62f71
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-22 09:19:40 -0400

    Add docs Beacon.

--- resources/views/nux/admin-page.php
+++ resources/views/nux/admin-page.php
@@ -36,3 +36,5 @@ wp_enqueue_style( 'bigbox-nux' );
 <div id="poststuff" class="wrap bigbox-setup-guide-steps">
 	<?php do_accordion_sections( 'bigbox-setup-steps', 'normal', null ); ?>
 </div>
+
+<script>!function(e,o,n){window.HSCW=o,window.HS=n,n.beacon=n.beacon||{};var t=n.beacon;t.userConfig={},t.readyQueue=[],t.config=function(e){this.userConfig=e},t.ready=function(e){this.readyQueue.push(e)},o.config={docs:{enabled:!0,baseUrl:"https://bigbox.helpscoutdocs.com/"},contact:{enabled:!1,formId:"e015d0b0-45ce-11e8-8d65-0ee9bb0328ce"}};var r=e.getElementsByTagName("script")[0],c=e.createElement("script");c.type="text/javascript",c.async=!0,c.src="https://djtflbt20bdde.cloudfront.net/",r.parentNode.insertBefore(c,r)}(document,window.HSCW||{},window.HS||{});</script>
