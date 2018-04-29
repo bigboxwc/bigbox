9e1fbd2e340a797717c191729e07561c375568fb
Spencer Finnell <spencerfinnell@gmail.com>
2018-04-28 20:59:01 -0400

    More tweaks for demo

--- app/integrations/woocommerce/wc-template-functions.php
+++ app/integrations/woocommerce/wc-template-functions.php
@@ -14,13 +14,51 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+/**
+ * Show subcategory thumbnails.
+ *
+ * @since 1.0.0
+ *
+ * @param mixed $category Category.
+ */
+function woocommerce_subcategory_thumbnail( $category ) {
+	// Begin modification.
+	$image        = false;
+	$image_srcset = false;
+	$image_sizes  = false;
+	// End modification.
+
+	$small_thumbnail_size = apply_filters( 'subcategory_archive_thumbnail_size', 'woocommerce_thumbnail' );
+	$dimensions           = wc_get_image_size( $small_thumbnail_size );
+	$thumbnail_id         = get_woocommerce_term_meta( $category->term_id, 'thumbnail_id', true );
+
+	if ( $thumbnail_id ) {
+		$image        = wp_get_attachment_image_src( $thumbnail_id, $small_thumbnail_size );
+		$image        = $image[0];
+		$image_srcset = function_exists( 'wp_get_attachment_image_srcset' ) ? wp_get_attachment_image_srcset( $thumbnail_id, $small_thumbnail_size ) : false;
+		$image_sizes  = function_exists( 'wp_get_attachment_image_sizes' ) ? wp_get_attachment_image_sizes( $thumbnail_id, $small_thumbnail_size ) : false;
+	}
+
+	if ( $image ) {
+		// Prevent esc_url from breaking spaces in urls for image embeds.
+		// Ref: https://core.trac.wordpress.org/ticket/23605.
+		$image = str_replace( ' ', '%20', $image );
+
+		// Add responsive image markup if available.
+		if ( $image_srcset && $image_sizes ) {
+			echo '<img src="' . esc_url( $image ) . '" alt="' . esc_attr( $category->name ) . '" width="' . esc_attr( $dimensions['width'] ) . '" height="' . esc_attr( $dimensions['height'] ) . '" srcset="' . esc_attr( $image_srcset ) . '" sizes="' . esc_attr( $image_sizes ) . '" />';
+		} else {
+			echo '<img src="' . esc_url( $image ) . '" alt="' . esc_attr( $category->name ) . '" width="' . esc_attr( $dimensions['width'] ) . '" height="' . esc_attr( $dimensions['height'] ) . '" />';
+		}
+	}
+}
+
 /**
  * Display product sub categories as thumbnails.
  *
- * This is a replacement for woocommerce_product_subcategories which also does some logic
- * based on the loop. This function however just outputs when called.
+ * @since 1.0.0
  *
- * @since 3.3.1
  * @param array $args Arguments.
  * @return boolean
  */
@@ -36,9 +74,11 @@ function woocommerce_output_product_categories( $args = array() ) {
 	$product_categories = woocommerce_get_product_subcategories( $args['parent_id'] );
 	$total              = count( $product_categories );
 
+	// Begin modification.
 	if ( $total > 5 ) {
 		$product_categories = array_slice( $product_categories, 0, 5 );
 	}
+	// End modification.
 
 	if ( ! $product_categories ) {
 		return false;
