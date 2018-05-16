4ade72c636327ddf425131339ef159cbb8d1a8d2
Spencer Finnell <spencerfinnell@gmail.com>
2018-05-16 10:04:13 -0400

    Simplify mobile menu logic. Closes #33

--- resources/assets/js/app/navbar-menu.js
+++ resources/assets/js/app/navbar-menu.js
@@ -1,107 +1,102 @@
 /* global jQuery */
 
-( function( $ ) {
-	const $mobile = $( '#navbar-mobile' );
-
+/**
+ * Better hover behavior.
+ */
+const addHoverIntent = () => {
 	/**
-	 * Create a separate clickable area on menu items with children on mobile.
+	 * Toggle menu classes when a hoverIntent has been toggled.
+	 *
+	 * @param {Object} $el Element that was hovered.
 	 */
-	const addChildToggles = () => {
-		$mobile.find( '.menu-item-has-children' ).each( function() {
-			$( this ).append( '<span class="mobile-menu-children-toggle"></span>' );
-		} );
+	const toggleClasses = function( $el ) {
+		$el
+			.toggleClass( 'menu-item-has-children--active' )
+			.parent()
+			.toggleClass( 'sub-menu--has-sibling' );
 	};
 
-	/**
-	 * Better hover behavior.
-	 */
-	const addHoverIntent = () => {
+	$( '#navbar-primary .menu-item-has-children' ).hoverIntent( {
 		/**
-		 * Toggle menu classes when a hoverIntent has been toggled.
-		 *
-		 * @param {Object} $el Element that was hovered.
+	 	 * When the mouse is over the element.
 		 */
-		const toggleClasses = function( $el ) {
-			$el
-				.toggleClass( 'menu-item-has-children--active' )
-				.parent()
-				.toggleClass( 'sub-menu--has-sibling' );
-		};
+		over: function() {
+			toggleClasses( $( this ) );
+		},
+		/**
+		 * When the mouse leaves the element.
+		 */
+		out: function() {
+			toggleClasses( $( this ) );
+		},
+		timeout: 200,
+		sensitivity: 7,
+		interval: 90,
+	} );
+};
 
-		$( '.menu-item-has-children' ).hoverIntent( {
-			/**
-			 * When the mouse is over the element.
-			 */
-			over: function() {
-				toggleClasses( $( this ) );
-			},
-			/**
-			 * When the mouse leaves the element.
-			 */
-			out: function() {
-				toggleClasses( $( this ) );
-			},
-			timeout: 200,
-			sensitivity: 7,
-			interval: 90,
+/**
+ * Touch support for tablets.
+ */
+const addTabletSupport = () => {
+	const $navigation = $( '#navbar-primary' ).find( '.navbar-menu__items' );
+
+	if ( ! $navigation.length ) {
+		return;
+	}
+
+	/**
+	 * Apply touch events to menu items.
+	 */
+	function toggleFocusTouchScreen() {
+		const $toggles = $navigation.find( '.menu-item-has-children > a' );
+
+		// Close when document is clicked close all menus.
+		$( document.body ).on( 'touchstart', ( e ) => {
+			if ( ! $( e.target ).closest( '.navbar-menu__items li' ).length ) {
+				$( '.navbar-menu__items li' ).removeClass( 'focus' );
+			}
 		} );
-	};
 
-	/**
-	 * Touch support for tablets.
-	 */
-	const addTabletSupport = () => {
-		const $navigation = $( '#navbar-primary' ).find( '.navbar-menu__items' );
+		// When the mobile menu toggle is not visible apply touch events to menu links.
+		if ( 'none' === $( '.navbar-mobile-toggle--open' ).css( 'display' ) ) {
+			$toggles.on( 'touchstart', function( e ) {
+				const $el = $( this ).parent( 'li' );
 
-		if ( ! $navigation.length ) {
-			return;
-		}
+				if ( ! $el.hasClass( 'focus' ) ) {
+					e.preventDefault();
 
-		/**
-		 * Apply touch events to menu items.
-		 */
-		function toggleFocusTouchScreen() {
-			const $toggles = $navigation.find( '.menu-item-has-children > a' );
-
-			// Close when document is clicked close all menus.
-			$( document.body ).on( 'touchstart', ( e ) => {
-				if ( ! $( e.target ).closest( '.navbar-menu__items li' ).length ) {
-					$( '.navbar-menu__items li' ).removeClass( 'focus' );
+					$el.toggleClass( 'focus' );
+					$el.siblings( '.focus' ).removeClass( 'focus' );
 				}
 			} );
-
-			// When the mobile menu toggle is not visible apply touch events to menu links.
-			if ( 'none' === $( '.navbar-mobile-toggle--open' ).css( 'display' ) ) {
-				$toggles.on( 'touchstart', function( e ) {
-					const $el = $( this ).parent( 'li' );
-
-					if ( ! $el.hasClass( 'focus' ) ) {
-						e.preventDefault();
-
-						$el.toggleClass( 'focus' );
-						$el.siblings( '.focus' ).removeClass( 'focus' );
-					}
-				} );
-			} else {
-				$toggles.unbind( 'touchstart' );
-			}
+		} else {
+			$toggles.unbind( 'touchstart' );
 		}
+	}
 
-		if ( 'ontouchstart' in window ) {
-			$( window ).on( 'resize', toggleFocusTouchScreen );
-			toggleFocusTouchScreen();
-		}
-	};
+	if ( 'ontouchstart' in window ) {
+		$( window ).on( 'resize', toggleFocusTouchScreen );
+		toggleFocusTouchScreen();
+	}
+};
 
-	addChildToggles();
+/**
+ * Mobile panels.
+ */
+const addMobilePanels = () => {
+	const className = 'menu-item-has-children--active';
+
+	$( '#navbar-mobile .menu-item-has-children' ).on( 'click', function( e ) {
+		e.stopPropagation();
+		$( this ).toggleClass( className );
+	} );
+};
+
+( function( $ ) {
 	addHoverIntent();
 	addTabletSupport();
-
-	// Toggle children.
-	$mobile.on( 'click', '.mobile-menu-children-toggle', function() {
-		$( this ).parent( '.menu-item-has-children' ).toggleClass( 'menu-item--opened' );
-	} );
-
-	// Add child toggles if needed.
-	$( document.body ).on( 'offCanvasDrawerSwap', addChildToggles );
+	addMobilePanels();
+	
+	$( document.body ).on( 'offCanvasDrawerSwap', addMobilePanels );
 }( jQuery ) );
