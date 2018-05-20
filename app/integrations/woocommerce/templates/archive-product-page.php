5399d929775a1e648ff1caea557af9daf7a57c1e
Spencer Finnell <spencerfinnell@gmail.com>
2018-05-20 10:39:25 -0400

    Remove duplicate hooks on dynamic shop page.

--- app/integrations/woocommerce/templates/archive-product-page.php
+++ app/integrations/woocommerce/templates/archive-product-page.php
@@ -29,53 +29,18 @@ get_header( 'shop' );
  */
 do_action( 'woocommerce_before_main_content' );
 
+while ( have_posts() ) :
+	the_post();
 ?>
+
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-	<?php endif; ?>
-
-	<?php
-	/**
-	 * Hook: woocommerce_archive_description.
-	 *
-	 * @hooked woocommerce_taxonomy_archive_description - 10
-	 * @hooked woocommerce_product_archive_description - 10
-	 */
-	do_action( 'woocommerce_archive_description' );
-	?>
+	<h1 class="woocommerce-products-header__title page-title"><?php the_title(); ?></h1>
 </header>
+
+<?php the_content(); ?>
+
 <?php
-if ( woocommerce_product_loop() ) {
-
-	/**
-	 * Hook: woocommerce_before_shop_loop.
-	 *
-	 * @hooked wc_print_notices - 10
-	 * @hooked woocommerce_result_count - 20
-	 * @hooked woocommerce_catalog_ordering - 30
-	 */
-	do_action( 'woocommerce_before_shop_loop' );
-
-	while ( have_posts() ) {
-		the_post();
-		the_content();
-	}
-
-	/**
-	 * Hook: woocommerce_after_shop_loop.
-	 *
-	 * @hooked woocommerce_pagination - 10
-	 */
-	do_action( 'woocommerce_after_shop_loop' );
-} else {
-	/**
-	 * Hook: woocommerce_no_products_found.
-	 *
-	 * @hooked wc_no_products_found - 10
-	 */
-	do_action( 'woocommerce_no_products_found' );
-}
+endwhile;
 
 /**
  * Hook: woocommerce_after_main_content.
