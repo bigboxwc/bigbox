2a2b10eaabc59e5799c6ff667dae9e3b0339c5b1
Spencer Finnell <spencerfinnell@gmail.com>
2018-06-11 11:26:17 -0400

    Add a specific class to the loop price div.

--- app/integrations/woocommerce/templates/loop/price.php
+++ app/integrations/woocommerce/templates/loop/price.php
@@ -27,7 +27,7 @@ $price_html = $product->get_price_html()
 
 <?php if ( $price_html ) : ?>
 
-<div class="product__meta">
+<div class="product__meta product__meta--price">
 	<?php woocommerce_show_product_loop_sale_flash(); ?>
 
 	<span class="price">
