d6ebf88193fb95b5cdbc13d9b5c11633792547a0
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-02 08:05:17 -0400

    Make custom color control more flexible by passing palettes to control registration.

--- app/customize/class-wp-customize-color-control.php
+++ app/customize/class-wp-customize-color-control.php
@@ -39,30 +39,6 @@ class WP_Customize_Color_Control extends \WP_Customize_Color_Control {
 	public $palettes = [];
 
 	/**
-	 * Register control.
-	 *
-	 * @since 1.8.0
-	 */
-	public function __construct( $manager, $id, $args = [] ) {
-		parent::__construct( $manager, $id, $args );
-
-		$palettes = [];
-		$colors   = bigbox_get_theme_colors();
-
-		foreach ( $colors as $theme_color => $color ) {
-			$palettes[] = bigbox_get_theme_color( $theme_color );
-		}
-
-		$palettes = array_slice( $palettes, 0, 5 );
-
-		$palettes[] = bigbox_get_theme_color( 'gray-200' );
-		$palettes[] = bigbox_get_theme_color( 'gray-400' );
-		$palettes[] = bigbox_get_theme_color( 'gray-700' );
-
-		$this->palettes = $palettes;
-	}
-
-	/**
 	 * Refresh the parameters passed to the JavaScript via JSON.
 	 *
 	 * @since 1.8.0
