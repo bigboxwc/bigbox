99146413bd3e768b9888beada4d54975a4b02734
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-05 18:36:26 -0400

    Remove more jQuery

--- resources/assets/js/app/navbar.js
+++ resources/assets/js/app/navbar.js
@@ -7,7 +7,7 @@ import domReady from '@wordpress/dom-ready';
  * Adjust width of navbar dropdown.
  */
 export const adjustWidth = () => {
-	const realSelect = document.querySelectorAll( '#search-dropdown-real select' );
+	const realSelect = document.querySelector( '#search-dropdown-real select' );
 	const fakeSelect = document.getElementById( 'search-dropdown-placeholder' );
 
 	if ( ! realSelect || ! fakeSelect ) {
@@ -15,13 +15,13 @@ export const adjustWidth = () => {
 	}
 
 	// Find visible option.
-	const selectedText = realSelect[0].options[ realSelect[0].selectedIndex ].text;
+	const selectedText = realSelect.options[ realSelect.selectedIndex ].text;
 
 	// Set hidden <select> to have only one value (of currently selected).
 	fakeSelect.innerHTML = `<option>${ selectedText.replace( '&nbsp;', '' ) }</option>`
 
 	// Set visible <select> to have width of hidden.
-	realSelect[0].style.width = `${ fakeSelect.offsetWidth }px`;
+	realSelect.style.width = `${ fakeSelect.offsetWidth }px`;
 
 	// Trigger again on change.
 	realSelect.onchange = adjustWidth;
---
+++ resources/assets/js/utils.js
@@ -0,0 +1,23 @@
+/**
+ * Determine if an element has a specific class.
+ *
+ * @param {object} el Node.
+ * @param {string} className Class name to search for.
+ */
+export const hasClass = ( el, className ) => {
+  if ( el.classList ) {
+    return el.classList.contains( className );
+  }
+
+  return new RegExp('(^| )' + str + '( |$)', 'gi').test( el.className );
+}
+
+/**
+ * Determine if an element has a specific class.
+ *
+ * @param {object} el Node.
+ * @param {string} className Class name to search for.
+ */
+export const isHidden = ( el ) => {
+  return el.offsetParent === null;
+}
