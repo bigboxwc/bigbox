4508a5360e508aded75a4ba6778042bd90a95f15
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-07 10:02:47 -0400

    File comments.

--- app/integrations/woocommerce/templates/checkout/cart.php
+++ app/integrations/woocommerce/templates/checkout/cart.php
@@ -1,3 +1,18 @@
+<?php
+/**
+ * Cart view in checkout.
+ *
+ * @since 1.0.0
+ *
+ * @package BigBox
+ * @category Theme
+ * @author Spencer Finnell
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
 
 <ul class="products products-main columns-1">
 	<?php do_action( 'woocommerce_review_order_before_cart_contents' ); ?>
