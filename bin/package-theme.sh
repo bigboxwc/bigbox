6aac0aa7fcd4ca97b2e30ff21c88771e88f95afd
Spencer Finnell <spencerfinnell@gmail.com>
2018-07-21 12:35:25 -0400

    Use wp-bin. See #70

--- bin/package-theme.sh
+++ bin/package-theme.sh
@@ -1,87 +1,28 @@
 #!/bin/bash
 
 # Include useful functions
-. "$(dirname "$0")/includes.sh"
+source "$(dirname "$0")/wp-bin/wp-bin.sh"
 
 # Exit if any command fails
 set -e
 
 # Change to the expected directory
-cd "$(dirname "$0")"
-cd ..
+go_to_root
 
-# Get version number from package.json
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g' \
-  | tr -d '[[:space:]]')
+PACKAGE_NAME="bigbox"
+PACKAGE_VERSION=$(get_package_version_number)
+PACKAGE_VERSION_PLACEHOLDER="BIGBOX_VERSION"
 
-# Make sure there are no changes in the working tree.  Release builds should be
-# traceable to a particular commit and reliably reproducible.
-changed=
-if ! git diff --exit-code > /dev/null; then
-	changed="file(s) modified"
-elif ! git diff --cached --exit-code > /dev/null; then
-	changed="file(s) staged"
-fi
-if [ ! -z "$changed" ]; then
-	git status
-	echo "ERROR: Cannot build theme zip with dirty working tree."
-	echo "       Commit your changes and try again."
-	exit 1
-fi
-
-branch="$(git rev-parse --abbrev-ref HEAD)"
-if [ "$branch" != 'master' ]; then
-	status "WARNING: You should probably be running this script against the 'master' branch (current: '$branch')"
-	echo
-	sleep 2
-fi
-
-# Do a dry run of the repository reset. Prompting the user for a list of all
-# files that will be removed should prevent them from losing important files!
-status "Resetting the repository to pristine condition."
-git clean -xdf --dry-run
-warning "About to delete everything above! Is this okay?"
-echo -n "[Y]es/[N]o: "
-
-read answer
-if [ "$answer" != "${answer#[Yy]}" ]; then
-	# Remove ignored files to reset repository to pristine condition. Previous
-	# test ensures that changed files abort the plugin build.
-	status "Cleaning working directory..."
-	git clean -xdf
-else
-	error "Aborting."
-	exit 0
-fi
-
-# Run the build
-status "Installing dependencies..."
-npm install
-composer install
-
-status "Creating language files..."
-wp i18n make-pot . resources/languages/bigbox.pot --domain=bigbox
-
-status "Updating Google Fonts..."
-npm run generate-font-list
-
-status "Generating build..."
-npm run build
+# Setup theme
+source "$(dirname "$0")/setup-theme.sh"
 
 # Update version in files.
-sed -i "" "s|%BIGBOX_VERSION%|${PACKAGE_VERSION}|g" style.css
-sed -i "" "s|%BIGBOX_VERSION%|${PACKAGE_VERSION}|g" functions.php
-
-# Remove any existing zip file
-rm -f bigbox*.zip
+sed -i "" "s|%${PACKAGE_VERSION_PACEHOLDER}%|${PACKAGE_VERSION}|g" style.css
+sed -i "" "s|%${PACKAGE_VERSION_PACEHOLDER}%|${PACKAGE_VERSION}|g" functions.php
 
 # Generate the theme zip file
-status "Creating archive..."
-zip -r bigbox.zip \
+status_message "Creating archive..."
+zip -r $PACKAGE_NAME.zip \
 	functions.php \
 	footer.php \
 	header.php \
@@ -100,10 +41,9 @@ zip -r bigbox.zip \
 	-x *.git*
 
 # Rename and cleanup.
-unzip bigbox.zip -d bigbox && zip -r "bigbox-$PACKAGE_VERSION.zip" bigbox
-rm -rf bigbox && rm -f bigbox.zip
+rezip_with_version $PACKAGE_NAME $PACKAGE_VERSION
 
 # Reset generated files.
 git reset head --hard
 
-success "📦 Version $PACKAGE_VERSION build complete."
+success_message "📦 Version $PACKAGE_VERSION build complete."
