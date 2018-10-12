68f7852c7c02f01105760d6f38cb0c7267a1d1b0
Spencer Finnell <spencerfinnell@gmail.com>
2018-10-12 11:52:38 -0400

    WooCommerce 3.5 template compatibility.

--- app/integrations/woocommerce/templates/cart/cart.php
+++ app/integrations/woocommerce/templates/cart/cart.php
@@ -13,14 +13,12 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.4.0
+ * @version 3.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-wc_print_notices();
 
 do_action( 'woocommerce_before_cart' ); ?>
 
