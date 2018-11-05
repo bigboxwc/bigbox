a1d57b2401738f53c5fdff64bf06d12f8e84aea7
Spencer Finnell <spencerfinnell@gmail.com>
2018-11-05 16:02:06 -0500

    Setup script for dev.

---
+++ bin/setup-theme-dev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Ensure we have access to wp-bin
+git submodule init
+git submodule update --recursive
+
+# Include useful functions
+source "$(dirname "$0")/wp-bin/wp-bin.sh"
+
+# Exit if any command fails
+set -e
+
+# Change to the expected directory
+go_to_root
+
+# Make sure there are no changes in the working tree.  Release builds should be
+# traceable to a particular commit and reliably reproducible.
+check_for_clean_cwd
+
+# Do a dry run of the repository reset. Prompting the user for a list of all
+# files that will be removed should prevent them from losing important files!
+reset_cwd
+
+# Run the build
+status_message "Installing dependencies..."
+npm install
+composer install
+composer run install-codestandards
+
+status_message "Creating language files..."
+wp i18n make-pot . resources/languages/bigbox.pot --domain=bigbox
+
+status_message "Updating Google Fonts..."
+npm run generate-font-list
+
+status_message "Building assets..."
+npm run dev
